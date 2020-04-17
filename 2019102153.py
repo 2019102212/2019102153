f605814d28ae67f4279c9f93ad3b1888ede5fc23
swa07016 <swa07016@naver.com>
2020-04-17 11:51:23 +0900

    1~9 times table

--- 2019102153.py
+++ 2019102153.py
@@ -1,6 +1,6 @@
-for i in range(1,9):
+for i in range(1,10):
     print(i,"단")
-    for j in range(1,9):
+    for j in range(1,10):
         print(i ,"x" ,j,"=",i*j,end="\t")
         if j%3==0:
             print()
